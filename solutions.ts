7bd121518fcdec77762d7cea5060b105dc00f013
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-09 21:28:49 +0600

    problem2

---
+++ solutions.ts
@@ -0,0 +1,9 @@
+function formatString(input: string, toUpper: boolean = true): string {
+  return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
