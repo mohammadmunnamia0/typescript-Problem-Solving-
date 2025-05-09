b38fd967bf34416da833ed6058638cbefe667b21
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-09 21:31:07 +0600

    problem6

--- solutions.ts
+++ solutions.ts
@@ -52,3 +52,16 @@ function processValue(value: string | number): number {
   }
   return value * 2;
 }
+
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) return null;
+  return products.reduce((max, current) =>
+    current.price > max.price ? current : max
+  );
+}
